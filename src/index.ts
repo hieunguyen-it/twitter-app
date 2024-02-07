7139d2e31358e13fcd7ff253a1eddcfae21864f0
Hieu Nguyen <hieunp@batgroup.vn>
2024-02-07 22:30:08 +0700

    feat: connect db

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import usersRouter from './routes/users.routes'
+import databaseService from './services/database.services'
 const app = express()
 
 const port = 3001
 
 app.use(express.json())
 app.use('/user', usersRouter)
+databaseService.connect()
 
 app.listen(port, () => {
   console.log('Example app listening on port 3000!')
